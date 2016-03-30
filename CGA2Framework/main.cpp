47ff3d1c40dddac7d1a1258df8171c28a1358453
Lars.Schloemer <lars.schloemer1@gmail.com>
2016-03-30 16:16:43 +0200

    stuff

---
+++ CGA2Framework/main.cpp
@@ -0,0 +1,42 @@
+#include <GL\glew.h>
+#include <glfw3.h>
+#include <glm/glm.hpp>
+
+
+int main(void)
+{
+
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+
+		/* Swap front and back buffers */
+		glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
